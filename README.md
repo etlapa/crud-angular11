0fa8a146ae194c11a9263d739b9494d00eea2d21
MARTIN TLAPA DAVILA <martintlapadavila@MARTINs-MacBook-Pro.local>
2021-02-05 09:58:55 -0600

    Step 2 — Generating Angular 11 CRUD Components and Service

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ CRUD example in angular based on
 
 https://medium.com/techiediaries-com/angular-11-crud-tutorial-with-example-9612aee3c32e
 
+Adding PDF with same instructions than link above
+angular-10-crud-bootstrap-tutorial.pdf
+
 Step so far:
 Step 1 — Creating a New Angular 11 Project
-
+Step 2 — Generating Angular 11 CRUD Components and Service
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCreateComponent,
+    ProductDetailsComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
